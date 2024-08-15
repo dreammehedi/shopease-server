d8669bf09bd4074c43768a5be55b177678861aa3
dreammehedi <dreammehedihassan@gmail.com>
2024-08-15 10:56:51 +0600

    Express Server Error Handler Added and Middleware Setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,40 @@
 const express = require("express");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const xssClean = require("xss-clean");
+const apiLimit = require("express-rate-limit");
+const apiCallLimit = apiLimit({
+  windowMs: 15 * 60 * 1000, // 10 minutes
+  max: 50,
+  message: "Too many requests from this IP, please try again later!",
+});
+const createError = require("http-errors");
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(xssClean());
+app.use(apiCallLimit);
+
+// home route
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("Welcome to the ShopEasy Server!");
+});
+
+// client error handler
+app.use((req, res, next) => {
+  next(createError(404, "Route not found!"));
+});
+
+// server error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
 });
 module.exports = app;
