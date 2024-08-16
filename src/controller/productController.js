f442a63b84896a0ebf1e776bc59066301f488df0
dreammehedi <dreammehedihassan@gmail.com>
2024-08-16 23:36:05 +0600

    Simple Pagination Added

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -2,8 +2,21 @@ const AllProducts = require("../model/productModel");
 
 // get all products
 const getAllProducts = async (req, res) => {
-  const allProducts = await AllProducts.find();
-  res.send(allProducts);
+  const currentPage = Number(req.query.currentPage);
+  console.log(currentPage);
+
+  const allProducts = await AllProducts.find()
+    .skip(currentPage * 6)
+    .limit(6);
+  const allProductsCount = await AllProducts.estimatedDocumentCount();
+
+  res.send({
+    success: true,
+    message: "All Products Geted Successfully.",
+    totalProductsCount: allProductsCount,
+    currentProductShowingCount: allProducts.length,
+    payload: allProducts,
+  });
 };
 
 module.exports = { getAllProducts };
